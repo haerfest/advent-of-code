928dfbf9af5f1d8119c2ebb2de925c39c9f8a910
haerfest <wouter.hobers@gmail.com>
2024-06-29 01:18:29 +0200

    Refactoring.

--- 2023/day01b.ml
+++ 2023/day01b.ml
@@ -30,28 +30,27 @@ let left_to_right =
 let right_to_left =
         List.map List.rev left_to_right
 
-let starts_with word chars =
+let starts_with word line =
         let rec inner wi ci wl cl =
                 if wi = wl then true
                 else if ci = cl then false
-                else if List.nth word wi <> List.nth chars ci then false
+                else if List.nth word wi <> List.nth line ci then false
                 else inner (wi + 1) (ci + 1) wl cl
         in
-        inner 0 0 (List.length word) (List.length chars)
+        inner 0 0 (List.length word) (List.length line)
 
-let rec first_word_of words chars =
-        if List.is_empty chars then raise InvalidInput
-        else match List.filter (fun word -> starts_with word chars) words with
+let rec first_word words line =
+        if List.is_empty line then raise InvalidInput
+        else match List.filter (fun word -> starts_with word line) words with
                 | word :: _ -> word
-                | [] -> first_word_of words (List.tl chars)
+                | [] -> first_word words (List.tl line)
 
 let solve sum line =
-        let chars = to_chars line in
-        let (chars_left, chars_right) = (first_word_of left_to_right chars, first_word_of right_to_left (List.rev chars)) in
+        let line' = to_chars line in
+        let (chars_left, chars_right) = (first_word left_to_right line', first_word right_to_left (List.rev line')) in
         let (word_left, word_right) = (to_word chars_left, to_word (List.rev chars_right)) in
         let (value_left, value_right) = (value word_left, value word_right) in
         sum + 10 * value_left + value_right 
-
 
 let () =
         Printf.printf "%d\n" (In_channel.with_open_text "input.txt" (In_channel.fold_lines solve 0))
