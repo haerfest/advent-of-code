f1dc67089401e281d490fffc614bc46b4ab3c70e
haerfest <wouter.hobers@gmail.com>
2024-07-04 23:33:54 +0200

    Refactoring.

--- 2023/day03b.ml
+++ 2023/day03b.ml
@@ -3,14 +3,14 @@ open Printf
 open String
 
 type item =
-  | Number of int * int * int * int
-  | Symbol of char * int * int
+  | Number of int * int * int
+  | Symbol of char * int
 
 let to_string = function
-  | Number (value, nr_digits, row, column) ->
-      sprintf "Number (%d, %d, %d, %d)" value nr_digits row column
-  | Symbol (character, row, column) ->
-      sprintf "Symbol ('%c', %d, %d)" character row column
+  | Number (value, nr_digits, position) ->
+      sprintf "Number (%d, %d, %d)" value nr_digits position
+  | Symbol (character, position) ->
+      sprintf "Symbol ('%c', %d)" character position
 
 let value_of character = Char.code character - Char.code '0'
 
@@ -21,25 +21,27 @@ let parse_number line column =
       let character = line.[column + nr_digits] in
       match character with
       | '0' .. '9' ->
-         let value = (value * 10) + value_of character in
+         let value = value * 10 + value_of character in
          parse value (nr_digits + 1)
       | _ -> (value, nr_digits)
   in
   parse 0 0
 
 let rec parse_line' line row column items =
-  if column = length line then items
+  let line_length = length line in
+  if column = line_length then items
   else
+    let position = row * line_length + column in
     let character = line.[column] in
     match character with
     | '.' ->
        parse_line' line row (column + 1) items
     | '0' .. '9' ->
        let value, nr_digits = parse_number line column in
-       let number = Number (value, nr_digits, row, column) in
+       let number = Number (value, nr_digits, position) in
        parse_line' line row (column + nr_digits) (number :: items)
     | _ ->
-       let symbol = Symbol (character, row, column) in
+       let symbol = Symbol (character, position) in
        parse_line' line row (column + 1) (symbol :: items)
 
 let parse_line line row items = parse_line' line row 0 items
