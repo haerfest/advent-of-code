681b543f3fb62223d604191eb19714a8c7341cb2
haerfest <wouter.hobers@gmail.com>
2024-07-05 23:23:43 +0200

    Complete day 3b.

--- 2023/day03b.ml
+++ 2023/day03b.ml
@@ -1,3 +1,4 @@
+open Hashtbl
 open In_channel
 open Printf
 open String
@@ -46,19 +47,58 @@ let rec parse_line' line row column items =
 
 let parse_line line row items = parse_line' line row 0 items
 
-let rec parse_lines' channel row items =
+let rec parse_lines' channel row line_length items =
   match input_line channel with
   | Some line ->
      let items = parse_line line row items in
-     parse_lines' channel (row + 1) items
-  | None -> items
+     parse_lines' channel (row + 1) (max (length line) line_length) items
+  | None -> (line_length, items)
 
-let parse_lines channel = parse_lines' channel 0 []
+let parse_lines channel = parse_lines' channel 0 0 []
+
+let store_numbers items =
+  let hash_table = create (List.length items) in
+  let store_number = function
+    | Number (value, nr_digits, position) ->
+       for key = position to position + nr_digits - 1 do
+         add hash_table key value
+       done
+    | _ -> ()
+  in
+  List.iter store_number items;
+  hash_table
+
+let part_numbers position n numbers =
+  let look_left_right p part_numbers =
+    match find_opt numbers p with
+    | Some a -> a :: part_numbers
+    | _ -> part_numbers
+  and look_up_down p part_numbers =
+    match (find_opt numbers (p - 1), find_opt numbers p, find_opt numbers (p + 1)) with
+    | (Some a, None, Some b) -> a :: b :: part_numbers
+    | (None, Some a, None)
+    | (Some a, _, _)
+    | (_, _, Some a) -> a :: part_numbers
+    | _ -> part_numbers
+  in      
+  []
+  |> look_left_right (position - 1)
+  |> look_left_right (position + 1)
+  |> look_up_down (position - n)
+  |> look_up_down (position + n)
 
 let solve channel =
-  let items = parse_lines channel in
-  let open List in
-  iter (fun item -> print_endline (to_string item)) items ;
-  length items
+  let (line_length, items) = parse_lines channel in
+  let numbers = store_numbers items in
+  let sum_gear_ratio sum = function
+    | Symbol ('*', position) ->
+       begin
+         match part_numbers position line_length numbers with
+         | [a; b] -> sum + a * b
+         | _ -> sum
+       end
+    | _ -> sum
+  in
+  List.fold_left sum_gear_ratio 0 items
 
 let () = printf "%d\n" (with_open_text "input.txt" solve)
